f9f4f2d3fbeee29c4b6f003c9585763806862e7c
gustavolohmannlinz <gustavolohmannlinz@gmail.com>
2022-08-16 18:35:44 -0300

    Atualizações de 16/08/2022

--- js/index.js
+++ js/index.js
@@ -21,3 +21,37 @@ function hideMenu() {
 }
 
 hideMenu();
+
+
+const imgCarrosuel = document.getElementById("img-carrosuel");
+const car = [
+    '../img-car/home-moderna-2.jpg',
+    '../img-car/home-moderna-3.jpg',
+    '../img-car/home-moderna-4.jpg',
+    '../img-car/home-moderna-5.jpg',
+    '../img-car/home-moderna-1.jpg'
+];
+const changeImgFirst = document.getElementById("change-img-first");
+const changeImgSecond = document.getElementById("change-img-second");
+let count = 0;
+let size = car.length;
+
+function carousel() {
+    if (size) {
+        count++;
+    }
+    if (count == size) {
+        count = 0;
+    }
+    imgCarrosuel.src = car[count];
+}
+
+changeImgSecond.addEventListener("click", () => {
+    carousel();
+    imgCarrosuel.classList.add("class-animation-slide");
+
+
+});
+//setInterval(, 2000);
+
+//setInterval(carousel,1000);
